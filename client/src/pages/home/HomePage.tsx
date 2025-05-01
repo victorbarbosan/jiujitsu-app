1919b2378b97a9be6447e9771575dedc6c011504
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2025-05-01 17:05:18 -0400

    Create HomePage

---
+++ client/src/pages/home/HomePage.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+
+
+// HomePage component
+export const HomePage = () => {
+    return (
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 8}}>
+            <p>Welcome to the Home Page!</p>
+        </Box>
+    )
+}
+
+export default HomePage;
