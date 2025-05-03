d5ef8f5ad0385cabfa763db9ef59d75c02506520
victorbarbosan <victorbnogueira@gmail.com>
2025-05-03 18:06:09 -0400

    Make Api test call

--- client/src/pages/home/HomePage.tsx
+++ client/src/pages/home/HomePage.tsx
@@ -1,11 +1,23 @@
 import { Box } from "@mui/material";
+import apiClient from "../../api/apiClient";
 
 
+const apiTest = async () => {
+    try {
+        const response = await apiClient.get('/')
+        console.log('API Response:', response.data);
+    }
+    catch (error) {
+        console.error('API Error:', error);
+    }
+}
+
+apiTest();
 
 // HomePage component
 export const HomePage = () => {
     return (
-        <Box sx={{ display: "flex", flexDirection: "column", gap: 8}}>
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 8 }}>
             <p>Welcome to the Home Page!</p>
         </Box>
     )
