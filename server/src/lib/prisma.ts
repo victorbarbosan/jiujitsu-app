1e777761ccf06f061fec079f75778735eb15bf15
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2025-06-02 19:39:11 -0400

    Create database successfully connected message

--- server/src/lib/prisma.ts
+++ server/src/lib/prisma.ts
@@ -1,7 +1,19 @@
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient({
-    log: process.env.NODE_ENV === 'development' ? ['query', 'info', 'warn', 'error'] : ['error']
+    log: process.env.NODE_ENV !== 'production'
+        ? ['query', 'info', 'warn', 'error']
+        : ['error'],
 })
 
+async function connectToDatabase() {
+    try {
+        await prisma.$connect()
+        console.log('✅ Database successfully connected')
+    } catch (e: any) {
+        console.error('❌ Failed to connect to the database:', e.message)
+    }
+}
+
+connectToDatabase()
 export default prisma
