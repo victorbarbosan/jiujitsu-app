21bd1a9780e3b20bba1adaf7361adbd6d0ee4564
Victor Barbosa <105869038+victorbarbosan@users.noreply.github.com>
2025-08-11 09:54:04 -0400

    Fetch users from the database

--- server/src/routes/users.routes.ts
+++ server/src/routes/users.routes.ts
@@ -1,12 +1,20 @@
 import { Router } from 'express';
+import { PrismaClient } from '@prisma/client';
 
+const prisma = new PrismaClient();
 
 const router = Router();
 
 // Get all users
-router.get('/', (req, res) => {
-    res.json({ message: "User 1, User 2, User 3" });
-    console.log(`GET request received at /api/users from ${req.ip}`);
+router.get('/', async (req, res) => {
+    try {
+        const users = await prisma.user.findMany();
+        res.json(users);
+        console.log(users)
+    } catch (error) {
+        console.error("Error fetching users:", error);
+        res.status(500).json({ error: "Failed to fetch users" });
+    }
 }
 );
 
